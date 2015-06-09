cb8767b9929cae97bd274cc887c8624ab60daffb
oscarw3 <oscarw3@gmail.com>
2015-06-09 16:09:25 +0200

    Updated Index and Using Twilio Testing

--- app/helpers/rides_helper.rb
+++ app/helpers/rides_helper.rb
@@ -7,14 +7,14 @@ module RidesHelper
 		# 3 = Ride Destroy (passengers)
 
   #Real Credentials
-      account_sid = 'AC818b91aaf3aa4883ebd967c1ce05b4f8'
-      auth_token = '635c887a39e2db3fffb74f31806d54d2'
-      from_number = "+14086414548"
+      # account_sid = 'AC818b91aaf3aa4883ebd967c1ce05b4f8'
+      # auth_token = '635c887a39e2db3fffb74f31806d54d2'
+      # from_number = "+14086414548"
       
   # Test Credentials
-  # account_sid = 'ACec40d0700dbd21edb9714b8b6ee492d6'
-  # auth_token = '9924fa846b8eff1d94faee71ac69eb5f'
-  # from_number = "+15005550006"
+  account_sid = 'ACec40d0700dbd21edb9714b8b6ee492d6'
+  auth_token = '9924fa846b8eff1d94faee71ac69eb5f'
+  from_number = "+15005550006"
   # Developer number +15005550006
   # Emergency code SjBcxP8IdNqX//QdKvr8+Ma6o/DSWQU6m0nprKfT
 		driver = User.find(ride.driverid)
